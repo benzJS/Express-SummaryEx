11c014dff961b435684c0613979b7146437b7084
benznt <tmno2tm@gmail.com>
2019-02-23 22:27:42 +0700

    add single product page

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,9 +2,14 @@ const User = require('../models/user.model');
 const Product = require('../models/product.model');
 
 module.exports.index = async function (req, res, next) {
-    let products = await Product.find();
+    const products = await Product.find();
     res.render('products', { 
         products: products,
         categories: req.baseUrl
     });
+}
+
+module.exports.single = async function(req, res, next) {
+	const single = await Product.findById(req.params.id);
+	res.render('single', { single: single }); 
 }
--- routes/product.route.js
+++ routes/product.route.js
@@ -5,4 +5,6 @@ const controller = require('../controllers/product.controller.js');
 
 router.get('/', controller.index);
 
+router.get('/:id', controller.single);
+
 module.exports = router;
