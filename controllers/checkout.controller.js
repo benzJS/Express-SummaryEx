4aee8aa0fb3ac583978cf2b752ff5f1e9978d226
benznt <tmno2tm@gmail.com>
2019-03-11 09:13:19 +0700

    configure checkout route

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -1,5 +1,6 @@
 module.exports.auth = function(req, res, next) {
 	if(req.signedCookies.userId) {
-	 return res.render('checkout')
+	 return res.render('checkout-auth');
 	}
+	return res.render('checkout-info');
 }
